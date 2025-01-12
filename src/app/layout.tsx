e25ceb9e015ce5110b176b30b3732c7bbb695351
Thamirys Gonçalves Santos <thamirys.goncalves@smartfastpay.com>
2025-01-12 01:02:12 -0300

    removed unused components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 import { JetBrains_Mono } from "next/font/google";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 // components
 import Header from "@/components/Header";
-import PageTransition from "@/components/PageTansition";
+import PageTransition from "@/components/PageTransition";
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ["latin"],
@@ -13,15 +12,15 @@ const jetbrainsMono = JetBrains_Mono({
   variable: "--font-jetbrainsMono",
 });
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Create Next App",
