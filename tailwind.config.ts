67c510f5c2b2d6d9d21da1b9f4464540773a8cc0
Thamirys Gonçalves Santos <thamirys.goncalves@smartfastpay.com>
2025-01-12 01:01:20 -0300

    changed accent color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,51 +1,51 @@
 import type { Config } from "tailwindcss";
 
 export default {
-    darkMode: ["class"],
-    content: [
+  darkMode: ["class"],
+  content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			primary: "#1c1c22",
-			accent: {
-				DEFAULT: '#00ff99',
-				hover: "#00e187",
-			},
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
-		screens: {
-			'sm': '640px',
-			'md': '768px',
-			'lg': '960px',
-			'xl': '1200px',
-			'2xl': '1536px',
-		},
-		fontFamily: {
-			primary: "var(--font-jetbrainsMono)",
-		},
-		// keyframes: {
-		// 	"accordion-down": {
-		// 		from:  { heigth: "0" },
-		// 		to: { heigth: "var(--radix-accordion-content-height)" },
-		// 	},
-		// 	"accordion-up": {
-		// 		from: { heigth: "var(--radix-accordion-content-height)" },
-		// 		to: { heigth: "0" },
-		// 	},
-		// },
-		// animation: {
-		// 	"accordion-down": "accordion-down 0.2s ease-out",
-		// 	"accordion-up": "accordion-up 0.2s ease-out"
-		// }
-  	}
+    extend: {
+      colors: {
+        primary: "#1c1c22",
+        accent: {
+          DEFAULT: "#ff1a00", // #00ff99
+          hover: "#e11700", //#00e187
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "960px",
+        xl: "1200px",
+        "2xl": "1536px",
+      },
+      fontFamily: {
+        primary: "var(--font-jetbrainsMono)",
+      },
+      // keyframes: {
+      // 	"accordion-down": {
+      // 		from:  { heigth: "0" },
+      // 		to: { heigth: "var(--radix-accordion-content-height)" },
+      // 	},
+      // 	"accordion-up": {
+      // 		from: { heigth: "var(--radix-accordion-content-height)" },
+      // 		to: { heigth: "0" },
+      // 	},
+      // },
+      // animation: {
+      // 	"accordion-down": "accordion-down 0.2s ease-out",
+      // 	"accordion-up": "accordion-up 0.2s ease-out"
+      // }
+    },
   },
   plugins: [require("tailwindcss-animate")],
 } satisfies Config;
