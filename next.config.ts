52e6bd2da3630083c16a274f16abb63ff32e1451
Thamirys Gonçalves Santos <thamirys.goncalves@smartfastpay.com>
2025-01-20 21:22:07 -0300

    testing for image asset on github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "https://github.com/ThamirysSantos/thamydev-protifolio.github.io",
+  assetPrefix: "./",
 };
 
 export default nextConfig;
